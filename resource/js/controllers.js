2ce16e92a16c8ff85c3af8fecf88a1cc7a902019
Ponecho <ponecho@gmail.com>
2018-12-04 14:48:45 +0100

    dev-clase2

--- resource/js/controllers.js
+++ resource/js/controllers.js
@@ -1,4 +1,9 @@
 angular.module("FinalApp")
-.controller("MainController", function ($scope) {
+.controller("MainController", function ($scope, $resource) {
+
+  Post=$resource('http//:jsonplaceholder.typicde/post/:id',{id:@id});
+  User=$resource('http//:jsonplaceholder.typicde/post/:id',{id:@id});
+  $scope.posts= Post.query();
+  $scope.users= User.query();
   
 })
