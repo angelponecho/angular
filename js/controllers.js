a24722a9eac2f4080aa234fc6d05cac60c6b0141
Ponecho <ponecho@gmail.com>
2018-12-12 14:56:36 +0100

    post

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,14 @@
 angular.module("FinalApp")
 .controller("MainController", function ($scope, $resource) {
 
-  Post=$resource('http//:jsonplaceholder.typicde/post/:id',{id:@id});
-  User=$resource('http//:jsonplaceholder.typicde/post/:id',{id:@id});
+  Post=$resource('http//:jsonplaceholder.typicode/post/:id',{id: @id});
+  User=$resource('http//:jsonplaceholder.typicode/post/:id',{id: @id});
   $scope.posts= Post.query();
   $scope.users= User.query();
   
 })
+
+  .controller("PostController", function ($scope,$resource, $routeParams) {
+    Post=$resource('http//:jsonplaceholder.typicode/post/:id',{id:@id});
+    $scope.posts= Post.get({id: $routeParams.id});
+  })
