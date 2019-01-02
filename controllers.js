96505b0c41e8bf64d789fc4be9aa748cb3661925
Ponecho <ponecho@gmail.com>
2019-01-02 10:58:34 +0100

    title

---
+++ controllers.js
@@ -0,0 +1,35 @@
+angular.module("FinalApp")
+.controller("MainController", function ($scope, $resource, PostResource) {
+  User=$resource('https://jsonplaceholder.typicode.com/users/:id',{id: "@id"});
+  $scope.posts= PostResource.query();
+  $scope.users= User.query();
+  $scope.removePost =function(post){
+        PostResource.delete({id: post.id},function(data){
+     })
+    $scope.post = $scope.post.filter(function(element){
+      return element.id!==post.id;
+    })
+  }
+
+})
+
+  .controller("PostController", function ($scope,$resource, $routeParams,$location) {
+    $scope.title='editar post';
+    $scope.posts= PostResource.get({id: $routeParams.id});
+    $scope.savePost =function(){
+      PostResource.save({id:$scope.post.id},{data:$scope.post}, function (data) {
+        $location.path("/post"+$scope.post.id)
+
+      })
+    }
+  })
+  .controller("NewPostController", function ($scope,PostResource,$location) {
+    $scope.posts= {};
+    $scope.title= 'Crear post';
+    $scope.savePost =function(){
+      PostResource.save({data:$scope.post},function (data) {
+        $location.path("/")
+
+      })
+    }
+  })
--- js/services.js
+++ js/services.js
@@ -1,4 +1,4 @@
-angular.module("finalApp")
+angular.module("FinalApp")
 .factory("postResource",function($resource){
 return $resource('https://jsonplaceholder.typicode.com/posts/:id',{id: "@id"},{update:{method:"put"}});
 
